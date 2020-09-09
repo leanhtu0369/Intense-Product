e0218a5236736949576ce04e444b749853f4771d
LeTu <leanhtu0369@gmail.com>
2020-09-09 14:12:38 +0700

    up demo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* JS script for giaidieu theme - made by giaidieu.com */
+(function ($, Drupal) {
+  Drupal.behaviors.giaidieu = {
+    attach: function (context, settings) {
+    }
+  };
+})
+(jQuery, Drupal);
